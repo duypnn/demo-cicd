b57bf73e8a79fe6cb972d827de055a484b563556
DuyPnn <duypnn@gmail.com>
2022-06-25 19:11:49 +0700

    update

--- codedeploy/start_server.sh
+++ codedeploy/start_server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # source /home/ec2-user/web/.env
-aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 434839081440.dkr.ecr.us-east-1.amazonaws.com
+aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 747872200924.dkr.ecr.ap-south-1.amazonaws.com
 cd /home/ec2-user/web/ && /usr/local/bin/docker-compose -f docker-compose.yml up -d
